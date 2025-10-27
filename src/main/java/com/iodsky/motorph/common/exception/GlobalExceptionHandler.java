f1c19cc301f7876df7beeac7fe44cba8c3b0ccfc
ibrahim <iharby2002@gmail.com>
2025-10-28 01:07:53 +0800

    Feat: add BadRequestException and enhance global exception handling
    - introduce BadRequestException for handling 400 errors.
    - refactor GlobalExceptionHandler to handle BadRequestException, AuthorizationDeniedException, and generic exceptions, providing appropriate HTTP status codes and error responses.

--- src/main/java/com/iodsky/motorph/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/iodsky/motorph/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.iodsky.motorph.common.exception;
 import com.iodsky.motorph.common.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,4 +25,23 @@ public class GlobalExceptionHandler {
         ErrorResponse error = new ErrorResponse(LocalDateTime.now(), 409, ex.getMessage(), null);
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
+        ErrorResponse error = new ErrorResponse(LocalDateTime.now(), 400, ex.getMessage(), null);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAuthorizationDeniedException(AuthorizationDeniedException ex) {
+        ErrorResponse error = new ErrorResponse(LocalDateTime.now(), 403, ex.getMessage(), null);
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllOtherException(Exception ex) {
+        ErrorResponse error = new ErrorResponse(LocalDateTime.now(), 500, ex.getMessage(), null);
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
