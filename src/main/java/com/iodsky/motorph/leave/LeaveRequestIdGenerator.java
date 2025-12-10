c56d7e2da0083210ea6f9c7191b3676fe10ad2f0
ibrahim <iharby2002@gmail.com>
2025-12-10 15:37:27 +0800

    Refactor: enhance LeaveRequest ID generation with UUID

--- src/main/java/com/iodsky/motorph/leave/LeaveRequestIdGenerator.java
+++ src/main/java/com/iodsky/motorph/leave/LeaveRequestIdGenerator.java
@@ -3,12 +3,16 @@ package com.iodsky.motorph.leave;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
+import java.util.UUID;
+
 public class LeaveRequestIdGenerator implements IdentifierGenerator {
 
     @Override
     public Object generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) {
         LeaveRequest leaveRequest = (LeaveRequest) o;
 
-        return leaveRequest.getRequestDate() + "-" + leaveRequest.getEmployee().getId();
+        return leaveRequest.getRequestDate() + "-" +
+                leaveRequest.getEmployee().getId() + "-" +
+                UUID.randomUUID().toString().substring(0, 8);
     }
 }
