3af419d4d709aebfa82066242710812464da9fff
ibrahim <iharby2002@gmail.com>
2026-01-16 16:03:25 +0800

    refactor: relocate OpenApiConfig and add soft delete filtering

--- src/main/java/com/iodsky/sweldox/common/BaseModel.java
+++ src/main/java/com/iodsky/sweldox/common/BaseModel.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.Instant;
 
 @MappedSuperclass
+@Where(clause = "deleted_at IS NULL")
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
--- src/main/java/com/iodsky/sweldox/common/OpenApiConfig.java
+++ src/main/java/com/iodsky/sweldox/common/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.iodsky.sweldox.security;
+package com.iodsky.sweldox.common;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
